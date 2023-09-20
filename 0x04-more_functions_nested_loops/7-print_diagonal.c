80662601c8cab6260cdbb9c86009fcde31593071
ReinaLalitta <lalittaruby@gmail.com>
2023-09-20 02:59:32 -0700

    Update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,22 +8,21 @@
 
 void print_diagonal(int n)
 {
-	int x, y;
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n > 1)
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+		_putchar('\\');
+		_putchar('\n');
+	}
 
 	if (n <= 0)
 	{
-	_putchar('\n');
-	}
-	else
-	{
-	for (x = 0; x < n; x++)
-	{
-	for (y = 0; y < n; y++)
-	{
-	_putchar(32);
-	}
-	_putchar(92);
-	_putchar('\n');
-	}
+		_putchar('\n');
 	}
 }
