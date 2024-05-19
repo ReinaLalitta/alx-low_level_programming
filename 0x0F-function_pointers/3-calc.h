eb9464eee32c2016f7f27eaa1d52cdebb16f078c
ReinaLalitta <lalittaruby@gmail.com>
2024-05-19 09:03:20 -0700

    Compilation and examples

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,13 +1,11 @@
 #ifndef _HEADER_
 #define _HEADER_
-
 /**
  * struct op - Struct op
  *
  * @op: The operator
  * @f: The function associated
  */
-
 typedef struct op
 {
 	char *op;
@@ -20,5 +18,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-
 #endif
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,75 +1,32 @@
 #include "3-calc.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 
 /**
-  * op_add - ...
-  * @a: ...
-  * @b: ...
+  * get_op_func - ...
+  * @s: ...
   *
   * Return: ...
   */
-int op_add(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
+	op_t ops[] = {
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
+	};
+	int i = 0;
 
-/**
-  * op_sub - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-
-/**
-  * op_mul - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
-  * op_div - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
-int op_div(int a, int b)
-{
-	if (b == 0)
+	while (i < 5)
 	{
-		printf("Error\n");
-		exit(100);
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+
+		i++;
 	}
 
-	return (a / b);
-}
-
-/**
-  * op_mod - ...
-  * @a: ...
-  * @b: ...
-  *
-  * Return: ...
-  */
-int op_mod(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	return (a % b);
+	return (0);
 }
